e40fe9876cbaf9f0e1b67265236f43686a4e645f
yosnak13 <yosnak13@gmail.com>
2020-06-01 23:23:26 +0900

    made contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -22,4 +22,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "contact | #{@base_title}"
+  end
+
 end
