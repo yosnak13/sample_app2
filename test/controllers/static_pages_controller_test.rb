021f1984eaa99d86340ec50bd159f28dc014511b
“yosnak13” <yosnak13@gmail.com>
2020-06-04 10:11:57 +0900

    適当にコミット

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,6 +6,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
+  
   test "should get root" do
     get root_url
     assert_response :success
