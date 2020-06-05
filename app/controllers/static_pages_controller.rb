77be26cd6391634c3aec5de845d0678c0c45ecdc
“yosnak13” <yosnak13@gmail.com>
2020-06-05 23:31:29 +0900

    passed test of list 5.2.6

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,8 +5,8 @@ class StaticPagesController < ApplicationController
   def help
   end
 
-  def about
-  end
+  # def about
+  # end
 
   def contact
   end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,37 +1,28 @@
 require 'test_helper'
 
-class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
-
-  
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
+class StaticPagesControllerTest < ActionController::TestCase
 
   test "should get home" do
-    get static_pages_home_url
+    get :home
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get :help
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
-  end
-  test "should get about" do
-    get static_pages_about_url
-    assert_response :success
-    assert_select "title", "About | #{@base_title}"
-  end
-  test "should get contact" do
-    get static_pages_contact_url
-    assert_response :success
-    assert_select "title", "contact | #{@base_title}"
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
 end
