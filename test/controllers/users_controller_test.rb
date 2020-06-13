106913b915a136dfb5ace6825ef344b76c3c4930
“yosnak13” <yosnak13@gmail.com>
2020-06-13 13:27:17 +0900

    strong parameters

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -27,4 +27,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to login_url
   end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {user: { password: @other_user.password,password_confirmation: @other_user.password,admin: true } }
+    assert_not @other_user.reload.admin?
+  end
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
 end
