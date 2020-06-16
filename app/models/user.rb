3f2405fc7d727f88a27548f4a30f5ef0132a8e47
“yosnak13” <yosnak13@gmail.com>
2020-06-16 20:14:35 +0900

    forgot password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
@@ -42,7 +42,8 @@ class User < ApplicationRecord
 
   # アカウントを有効にする
   def activate
-    update_columns(activated: true, activated_at: Time.zone.now)
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
   end
 
   # 有効化用のメールを送信する
@@ -50,11 +51,23 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
+  # パスワード再設定の属性を設定する
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # パスワード再設定のメールを送信する
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+  
   private
 
   # メールアドレスをすべて小文字にする
   def downcase_email
-    email.downcase!
+    self.email = email.downcase
   end
 
   # 有効化トークンとダイジェストを作成および代入する
@@ -62,5 +75,4 @@ class User < ApplicationRecord
     self.activation_token  = User.new_token
     self.activation_digest = User.digest(activation_token)
   end
-
 end
