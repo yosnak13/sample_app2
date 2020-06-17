c611759d32d21563dbb97a6926086eee782b04a8
“yosnak13” <yosnak13@gmail.com>
2020-06-17 18:47:49 +0900

    production.rb is fixed agaim

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'pacific-reef-16070.herokuapp.com'
+  host = 'http://pacific-reef-16070.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
